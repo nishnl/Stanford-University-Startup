6e5d1ad7643e45b18b1e455781ef86bd114fae1f
Nish <nishnl@hotmail.com>
2013-06-01 17:03:47 -0400

    Add Paperclip Gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601195432) do
+ActiveRecord::Schema.define(:version => 20130601203814) do
 
   create_table "projects", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
