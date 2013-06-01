df94a8f22f6ec5b68c6f61ac6ee822bb8dc7a9c7
Nish <nishnl@hotmail.com>
2013-06-01 02:06:17 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stanfordiot::Application.config.secret_token = 'b90274f0ea55f2e959ec21be981a9513621cd5b7928cb8b716d899ffd1464770f5c4d27fd4bd40773e7ef11a64afe1b3139f0f14171818eeddc96902f67dc127'
